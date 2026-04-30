b86007c6a1a32f11c63975df9e1a84cd36828c94
Vignesh Raj D <dv000000@risk.regn.net>
2026-04-30 15:05:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "azurerm_storage_account" "sa" {
   sftp_enabled                      = var.enable_sftp
   large_file_share_enabled          = var.enable_large_file_share
   allow_nested_items_to_be_public   = var.allow_nested_items_to_be_public
-  https_traffic_only_enabled        = var.enable_https_traffic_only
+  enable_https_traffic_only         = var.enable_https_traffic_only
   min_tls_version                   = var.min_tls_version
   nfsv3_enabled                     = var.nfsv3_enabled
   infrastructure_encryption_enabled = var.infrastructure_encryption_enabled
